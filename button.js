d14734605fc1461b05de493c21224ac6477cea2b
Your Namemaksimovegor401@gmail.com <you@example.com>
2022-07-13 15:44:51 +0300

    work3

--- button.js
+++ button.js
@@ -1,13 +1,14 @@
 const menuBtn = document.querySelector('.menu-btn');
 const menu = document.querySelector(".header__mobile-navigation");
 const body = document.querySelector("body");
+const mainBox = document.querySelector("mainBox");
 let menuOpen = false;
 
 menuBtn.addEventListener('click', () => {
   if(!menuOpen) {
     menuBtn.classList.add('open');
-    menu.classList.add("visible")
-    body.classList.add("block-scroll")
+    menu.classList.add("visible");
+    body.classList.add("block-scroll");
     menuOpen = true;
   } else {
     menuBtn.classList.remove('open');
